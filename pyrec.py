ea42b069cf7508fe56f3167e30f0ff6d0c6ec854
INQUIRER-LAB <tayez788@gmail.com>
2022-01-20 00:54:36 +0900

    2乗を消す

--- pyrec.py
+++ pyrec.py
@@ -23,22 +23,24 @@ sound_L = 0
 sound_R = 0
 # 遅延時間
 leatancy = 0
+cr0 = 0
+cr1 = 0
 ########################
 
 # 録音した音声データの取り出し（コールバック）
 def cb_audio_proc(in_data, frame_count, time_info, status):
-    global sound_L, sound_R, start_L, start_R, leatancy
+    global sound_L, sound_R, start_L, start_R, leatancy, cr0, cr1
     x = np.frombuffer(in_data, dtype = np.int16).reshape(CHUNK, CHANNELS).T # frombufferで高速に16bitデータ取り出し(スキャン方向に注意)
     t = (time.perf_counter_ns()-T0)/(10**9)
     
     # 原音の開始取得
-    r0 = (x[1][0])**2
+    r0 = x[1][0]
     if r0 > 0.5 and start_L == 0:
             sound_L = t
             start_L += 1
 
     # 受信音の開始取得
-    r1 = (x[0][0])**2
+    r1 = x[0][0]
     if r1 > 0.5 and start_R == 0:
             sound_R = t
             start_R += 1
@@ -47,7 +49,9 @@ def cb_audio_proc(in_data, frame_count, time_info, status):
     if sound_L != 0 and sound_R != 0:
         latancy = sound_R-sound_L
         print('\r%10.3f[sec]' % (latancy), end = '')
-        
+    else:
+        print('\r   処理中@_@  ',end='')
+    print('\r%4d'%cr0,end='')
     return None, pyaudio.paContinue
 
 # メインルーチン
